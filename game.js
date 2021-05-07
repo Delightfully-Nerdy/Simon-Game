c1ee04d7caa6c0e7b4849e3645bdccc67aa81934
DalitsoKasonde <dkasonde11@gmail.com>
2021-05-07 14:29:23 +0200

    incomplete solution

---
+++ game.js
@@ -0,0 +1,11 @@
+var gamePattern = [];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+nextSequence () {
+    var n = (Math.random()) * 3;
+
+    var randomNumber = Math.floor(n);
+}
+
+var randomChosenColor 
