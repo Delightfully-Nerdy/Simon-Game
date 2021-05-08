b2c31deef39940496cafec1572d38924d57d37e4
DalitsoKasonde <dkasonde11@gmail.com>
2021-05-08 04:51:46 +0200

    solution to play audio

--- game.js
+++ game.js
@@ -8,4 +8,12 @@ function nextSequence() {
   var randomChosenColor = buttonColours[randomNumber];
 
   gamePattern.push(randomChosenColor);
+
+  $("#" + randomChoosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
 }
